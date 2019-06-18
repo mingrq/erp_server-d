436659be994092d9881a2956f53260623ab23ce0
002 <mingruqi@sina.cn>
2019-06-18 18:19:03 +0800

    找到单元测试的使用方法

--- common/organization/src/main/java/com/ming/organization/dao/Organization.java
+++ common/organization/src/main/java/com/ming/organization/dao/Organization.java
@@ -8,30 +8,55 @@ import java.util.List;
 /**
  * 组织信息
  */
-
-
 public interface Organization {
-    /*添加组织*/
+    /**
+     * 添加组织
+     * @param entity 组织实体
+     */
     void addOrganization(OrganizationEntity entity);
 
-    /*删除组织*/
+    /**
+     * 删除组织
+     * @param organizationId 组织id
+     */
     void delectOrganization(int organizationId);
 
-    /*修改组织*/
+    /**
+     * 修改组织
+     * @param entity 组织实体
+     */
     void alertOrganization(OrganizationEntity entity);
 
-    /*查询组织*/
+    /**
+     * 查询组织
+     * @param organizationId 组织id
+     */
     OrganizationEntity getOrganization(int organizationId);
 
-    /*查询所有组织*/
+    /**
+     * 查询所有组织
+     * @return List<OrganizationEntity>集合
+     */
     List<OrganizationEntity> getOrganizationList();
 
-    /*查询组织名称*/
+    /**
+     * 查询组织名称
+     * @param organizationId 组织id
+     * @return 组织名称
+     */
     String getOrganizationName(int organizationId);
 
-    /*查询组织创建时间*/
+    /**
+     * 查询组织创建时间
+     * @param organizationId 组织id
+     * @return 创建时间
+     */
     String getOrganizationGenTime(int organizationId);
 
-    /*查询上级组织id*/
+    /**
+     * 查询上级组织id
+     * @param organizationId 组织id
+     * @return 上级组织id
+     */
     int getOrganizationSuperiorId(int organizationId);
 }
--- common/organization/src/main/java/com/ming/organization/service/OrganizationService.java
+++ common/organization/src/main/java/com/ming/organization/service/OrganizationService.java
@@ -5,38 +5,86 @@ import com.ming.organization.entity.OrganizationEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class OrganizationService {
 
     @Autowired
     private Organization organization;
 
-    /*添加组织*/
+    /**
+     * 添加组织
+     *
+     * @param entity 组织实体
+     */
     public void addOrganization(OrganizationEntity entity) {
         organization.addOrganization(entity);
     }
 
-    /*删除组织*/
+    /**
+     * 删除组织
+     *
+     * @param organizationId 组织id
+     */
     public void delectOrganization(int organizationId) {
+        organization.delectOrganization(organizationId);
     }
 
-    /*修改组织*/
+    /**
+     * 修改组织
+     *
+     * @param entity 组织实体
+     */
     public void alertOrganization(OrganizationEntity entity) {
+        organization.alertOrganization(entity);
     }
 
-    /*修改组织名称*/
-    public void alertOrganizationName(int organizationId) {
-    }
-
-    /*查询组织*/
+    /**
+     * 查询组织
+     *
+     * @param organizationId 组织id
+     */
     public void getOrganization(int organizationId) {
-
+        organization.getOrganization(organizationId);
     }
 
-    /*查询组织名称*/
+    /**
+     * 查询所有组织
+     *
+     * @return List<OrganizationEntity>集合
+     */
+    public List<OrganizationEntity> getOrganizationList() {
+        return organization.getOrganizationList();
+    }
+
+    /**
+     * 查询组织名称
+     *
+     * @param organizationId 组织id
+     * @return 组织名称
+     */
     public String getOrganizationName(int organizationId) {
         return organization.getOrganizationName(organizationId);
     }
 
-    /*查询所有组织*/
+    /**
+     * 查询组织创建时间
+     *
+     * @param organizationId 组织id
+     * @return 创建时间
+     */
+    public String getOrganizationGenTime(int organizationId) {
+        return organization.getOrganizationGenTime(organizationId);
+    }
+
+    /**
+     * 查询上级组织id
+     *
+     * @param organizationId 组织id
+     * @return 上级组织id
+     */
+    public int getOrganizationSuperiorId(int organizationId) {
+        return organization.getOrganizationSuperiorId(organizationId);
+    }
 }
