f0649f6e4c010dcc091995b5ec8e2396081fa333
002 <mingruqi@sina.cn>
2019-06-26 18:14:15 +0800

    同步

---
+++ common/com-common/src/main/java/com/ming/company/user/aop/GatherUserInfoAspect.java
@@ -0,0 +1,13 @@
+package com.ming.company.user.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * 收集用户信息AOP
+ */
+
+@Aspect
+@Component
+public class GatherUserInfoAspect {
+}
